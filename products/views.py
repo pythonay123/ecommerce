622fee51eb93204f07e41fb0f2af5ffbb4761d5e
Ayoola Dogo <pythonworkforay@gmail.com>
2019-12-26 15:51:29 +0000

    second commit

--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 
 # Create your views here.
 def home(request):
-    template = 'base.html'
-    context = locals()
+    if request.user.is_authenticated:
+        #     user_name = 'Justin is the current user'
+        # else:
+        #     user_name = 'Unknown'
+        user_name = request.user
+    else:
+        user_name = request.user
+    context = {'user_name': user_name}
+    template = 'products/home.html'
     return render(request, template, context)
